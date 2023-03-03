59f2ccb770beb9e96eb7dad5a5ca184a1bb22f61
Jinay Jain <jinaybjain@gmail.com>
2023-03-02 20:01:55 -0500

    Add reactor code

--- ws/src/wanderer/wanderer/reactor.py
+++ ws/src/wanderer/wanderer/reactor.py
@@ -10,26 +10,71 @@ from rclpy import qos
 from math import radians
 from random import uniform
 
+
+FORWARD_DETECTION_THRESHOLD = 10
+TURNAROUND_THRESHOLD = 500
+TURNING_POWER = 0.01
+TIMER_INTERVAL = 0.5
+
+
 class Reactor(Node):
 
     def __init__(self):
         super().__init__('reactor')
-        self.hazard_subscription = self.create_subscription(
+        self.ir_subscription = self.create_subscription(
             IrIntensityVector,
             'zelda/ir_intensity',
             self.ir_callback,
             qos.qos_profile_sensor_data)
 
         self.publisher = self.create_publisher(Twist, 'zelda/cmd_vel', 10)
-
         self.move_timer = self.create_timer(
             TIMER_INTERVAL,
             self.move_timer_callback
         )
-    #def hazard_callback(self, haz: IrIntensityVector):
+        self.twist = Twist()
 
-        #for det in haz.:
+        self.spin_timer = None
 
+    def move_timer_callback(self):
+        if self.spin_timer is not None:
+            self.twist = Twist()
+            self.twist.angular.z = 1
+
+        self.publisher.publish(self.twist)
+
+    def spin_timer_callback(self):
+        self.twist = Twist()
+        self.spin_timer.destroy()
+        self.spin_timer = None
+
+    def ir_callback(self, ir: IrIntensityVector):
+        if self.spin_timer is not None:
+            return
+
+        self.get_logger().info(f"ir_callback: {ir.readings}")
+
+        readings = [float(r.value) for r in ir.readings]
+
+        left_average = sum(readings[:3]) / 3
+        middle_average = sum(readings[2:5]) / 3
+        right_average = sum(readings[4:]) / 3
+
+        difference = right_average - left_average
+
+        if middle_average < FORWARD_DETECTION_THRESHOLD:
+            difference = 0
+        elif middle_average > TURNAROUND_THRESHOLD:
+            # turn around
+            self.get_logger().info("turning around")
+            self.spin_timer = self.create_timer(
+                radians(180),
+                self.spin_timer_callback
+            )
+            return
+
+        self.twist.angular.z = max(min(TURNING_POWER * difference, 1), -1)
+        self.twist.linear.x = 0.1
 
 
 def main(args=None):
